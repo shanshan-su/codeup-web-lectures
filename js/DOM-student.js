e430e23b9809c19327ee2b7288f24d681d86e4a5
Shanshan Su <shanshansu36@gmail.com>
2021-03-29 15:02:37 -0500

    feat: finished DOM-student exercise in class

--- js/DOM-student.js
+++ js/DOM-student.js
@@ -47,20 +47,59 @@ console.log(locateElements("li"));
 
 // TODO: Howell's doesn't sound like the name you'd give your hat shop - let's change the innerText of that in our navigation bar to your own name.
 
+var navElements = document.getElementsByClassName("navbar-brand");
+navElements[0].innerHTML = "Su's <i class=\"bi-shop mx-1\"></i>"
+
 // TODO: There's only one ordered list on the page - can you get into that ordered list and use innerHTML to change the list items within that ordered list to what you think the top sellers were?
 
+var orderedListItems = document.getElementsByTagName("ol");
+orderedListItems[0].innerHTML = "<li>Coonskin cap</li><li>Chullo hats</li><li>Catwheel hats</li><li>Capirote hats</li>"
+
 // TODO (BONUS): I have two "special-offer" classes used in my table - grab those classes by name, store them into a variable, and then use a loop to replace the innerHTML. The new table data should have strings that let our customers know that "New discounts are coming soon!"
+
+var specialOfferItems = document.getElementsByClassName("special-offer");
+
+for (i = 0; i < specialOfferItems.length; i++) {
+    specialOfferItems[i].innerHTML = "New discounts are coming soon!";
+}
 
 // attributes
 
 // TODO: Bringing in Bootstrap components, sometimes we can forget to change example or demo related text - one of my ids in the HTML is set to "navbarTogglerDemo02". I don't think we need that ID at all, can you remove that attribute for me? Start by storing it in a variable!
 
+var navbarToggler = document.getElementById("navbarTogglerDemo02");
+navbarToggler.removeAttribute("id");
+
 // TODO: Uh oh - actually there was a BUTTON that referred to 'navbarTogglerDemo02' that is right above it in our HTML structure! Let's add an ID back to the variable you used above that makes more sense (perhaps "navbar-collapser"?)
 
+navbarToggler.setAttribute("id", "navbar-collapser");
+
 // TODO: Now, get into that button element that makes reference to our old "navbarTogglerDemo02" ids and change those attributes to match. Refactor complete!
+
+var btnNavbarToggler = document.getElementsByClassName("navbar-toggler");
+btnNavbarToggler[0].setAttribute("aria-controls", "navbar-collapser");
 
 // style
 
 // TODO: I've embedded a style sheet that adjusts color for the body, a class called "main-content", and an ID of "top-header". Target the three of these elements and set your own colors as you'd like to each of the three I've mentioned.
+var bodyElement = document.getElementsByTagName("body")[0];
+var mainContentElement = document.getElementsByClassName("main-content")[0];
+var topHeaderElement = document.getElementById("top-header");
+
+bodyElement.style["background-color"] = "#D6798F";
+mainContentElement.style.color = "#574B4E";
+mainContentElement.style["background-color"] = "#DDC0C7";
+topHeaderElement.style.color = "#DDC0C7";
+topHeaderElement.style["background-color"] ="#57313A";
 
 // TODO (BONUS): That default bootstrap font is nice, but we've been adding our own fonts to thing for a couple of weeks now! Add a font-family of some kind that you'd like it to adjust to.
+
+bodyElement.style.fontFamily = "Open Sans";
+topHeaderElement.style.fontFamily = "Merriweather";
+
+var h4Elements = locateElements("h4");
+
+for (i = 0; i < h4Elements.length; i++) {
+    h4Elements[i].style.fontFamily = "Roboto";
+}
+
